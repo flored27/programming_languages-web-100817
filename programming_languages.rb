e57c7c51ec911ee704a194c105e63e4fad494a3f
flored27 <dflores@fandm.edu>
2017-10-05 12:46:28 -0400

    interesting

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  newhash = Hash.new
+  languages.each do |style, data|
+    data.each do |program, data2|
+      data2.each do |type, thetype|
+        newhash[:"#{program}"]={type:"#{thetype}", style:[]}
+        if languages.dig(:functional).include?(:"#{program}") && languages.dig(:oo).include?(:"#{program}")
+          newhash[:"#{program}"][:style].push(:functional, :oo)
+
+          elsif languages.dig(:oo).include?(:"#{program}")
+          newhash[:"#{program}"][:style] << :oo
+
+        elsif languages.dig(:functional).include?(:"#{program}")
+          newhash[:"#{program}"][:style] << :functional
+
+      end
+    end
+    end
+  end
+return newhash
 end
